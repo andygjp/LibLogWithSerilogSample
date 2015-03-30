623751f7800c3f4c436554ef92fa7f290814f977
Andrew Pattison <andrew.pattison@brightfuture.co.uk>
2015-03-30 11:33:27 +0100

    Initial commit

---
+++ LibLogWithSerilogSample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LibLogWithSerilogSample
+{
+    using ConsoleApplication7.Logging;
+    using Serilog;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration().ReadAppSettings().CreateLogger();
+
+            var logger = LogProvider.GetCurrentClassLogger();
+            logger.Debug(() => "write value to log.");
+        }
+    }
+}
